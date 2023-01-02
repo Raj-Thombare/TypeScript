cb327ba189029124390321e08ca2d155ae687d42
Rakaa <therajthombare@gmail.com>
2023-01-02 17:11:39 +0530

    basic types done

--- pure-ts/app.js
+++ pure-ts/app.js
@@ -4,3 +4,16 @@ var inputs = document.querySelectorAll("input");
 inputs.forEach(function (input) {
     console.log(input);
 });
+// Basic Types
+var ac = "Raj"; // Type inference feature allow to define type without explicitely defining it!
+var age = 21;
+var isMale = true;
+// isMale = "yes";  Type 'string' is not assignable to type 'boolean'
+var eligible = null;
+// eligible = "not valid"; since it type defined is null, it doesn't make any sence to assign something to it!
+function sum(a, b) {
+    // Union type makes it possible for variables to have more than one type eg. number | string
+    // TypeScript has a default type any. when you don't explicitely define types then TS implicitely set type to any.
+    return a + b;
+}
+console.log(sum("2", 9)); // 29
--- pure-ts/app.ts
+++ pure-ts/app.ts
@@ -7,3 +7,21 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
   console.log(input);
 });
+
+// Basic Types
+
+let ac = "Raj"; // Type inference feature allow to define type without explicitely defining it!
+let age = 21;
+let isMale = true;
+// isMale = "yes";  Type 'string' is not assignable to type 'boolean'
+
+let eligible = null;
+// eligible = "not valid"; since it type defined is null, it doesn't make any sence to assign something to it!
+
+function sum(a, b: number): number | string {
+  // Union type makes it possible for variables to have more than one type eg. number | string
+  // TypeScript has a default type any. when you don't explicitely define types then TS implicitely set type to any.
+  return a + b;
+}
+
+console.log(sum("2", 9)); // 29
